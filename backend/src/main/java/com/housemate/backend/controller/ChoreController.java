d75b180dbfa56aadded51c86d7d6791a9db4e206
Mattia A <mattia.amati@gmail.com>
2026-03-03 15:38:05 +0100

    feat: added config packages for both client and backend modules to provide logging and a unified base url; also implemented the first ChoreService method

--- backend/src/main/java/com/housemate/backend/controller/ChoreController.java
+++ backend/src/main/java/com/housemate/backend/controller/ChoreController.java
@@ -1,5 +1,34 @@
 package com.housemate.backend.controller;
+import com.housemate.backend.service.ChoreService;
+import com.housemate.shared.dto.chore.request.ChoreCreateRequestDTO;
+import com.housemate.shared.dto.chore.response.ChoreResponseDTO;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/chores")
-public class ChoreController {}
+public class ChoreController {
+
+    private final ChoreService choreService;
+
+    public ChoreController(ChoreService choreService) {
+        this.choreService = choreService;
+    }
+
+    @PostMapping
+    public ChoreResponseDTO createChore(ChoreCreateRequestDTO choreCreateRequestDTO) {
+
+        return choreService.createChore(choreCreateRequestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public String updateChore(@PathVariable UUID id) {
+        return "Update chore with ID: " + id;
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteChore(@PathVariable UUID id) {
+        return "Delete chore with ID: " + id;
+    }
+}
--- backend/src/main/java/com/housemate/backend/service/ChoreService.java
+++ backend/src/main/java/com/housemate/backend/service/ChoreService.java
@@ -1,6 +1,41 @@
 package com.housemate.backend.service;
+import com.housemate.backend.model.chore.Chore;
+import com.housemate.backend.model.household.Household;
+import com.housemate.backend.repository.chore.ChoreRepository;
+import com.housemate.backend.repository.household.HouseholdRepository;
+import com.housemate.shared.dto.chore.request.ChoreCreateRequestDTO;
+import com.housemate.shared.dto.chore.response.ChoreResponseDTO;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
-public class ChoreService {}
+public class ChoreService {
+
+    private final ChoreRepository choreRepository;
+    private final HouseholdRepository householdRepository;
+
+    @Transactional
+    public ChoreResponseDTO createChore(ChoreCreateRequestDTO dto) {
+        Household household = householdRepository.findById(dto.householdId())
+                              .orElseThrow(()  -> new IllegalArgumentException("Household with ID: "
+                                                                               + dto.householdId() +
+                                                                               " not found."));
+
+        Chore newChore = new Chore();
+        newChore.setHousehold(household);
+        newChore.setFrequency(dto.frequencyDays());
+        newChore.setDescription(dto.description());
+
+        Chore savedChore = choreRepository.save(newChore);
+
+        return new ChoreResponseDTO(savedChore.getId(),
+                                    savedChore.getDescription(),
+                                    savedChore.getFrequency());
+
+    }
+}
