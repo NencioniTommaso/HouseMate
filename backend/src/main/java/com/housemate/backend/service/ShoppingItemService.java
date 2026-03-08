a1fffd3e22187c75253f9cd02527432ecd64f051
Mattia A <mattia.amati@gmail.com>
2026-03-08 11:52:31 +0100

    feat: added empty files for ShoppingItemController and ShoppingItemService

---
+++ backend/src/main/java/com/housemate/backend/service/ShoppingItemService.java
@@ -0,0 +1,11 @@
+package com.housemate.backend.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ShoppingItemService {
+
+
+}
