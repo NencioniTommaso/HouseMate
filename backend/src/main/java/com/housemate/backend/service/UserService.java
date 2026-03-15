d6ce112b6ced40975fb8f4f50ba53942b133bdad
PaudiDev <Simone.paudice@gmail.com>
2026-03-16 00:30:47 +0100

    fix: enforced validation in UserService for JWT related methods

--- backend/src/main/java/com/housemate/backend/service/UserService.java
+++ backend/src/main/java/com/housemate/backend/service/UserService.java
@@ -2,13 +2,16 @@ package com.housemate.backend.service;
 
 import com.housemate.backend.model.user.User;
 import com.housemate.backend.repository.user.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.lang.NonNull;
+import org.springframework.util.Assert;
 
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -19,24 +22,33 @@ public class UserService implements UserDetailsService {
 
     // Used for login authentication by Spring Security
     @Override
+    @NonNull
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
-        return org.springframework.security.core.userdetails.User.builder()
+        return Objects.requireNonNull(
+            org.springframework.security.core.userdetails.User.builder()
                 .username(user.getId().toString()) 
                 .password(user.getPassword())
-                .build();
+                .build(),
+            "unexpectedly built a null UserDetails object for email: " + email
+        );
     }
 
     // Used for JWT authentication, password is not needed as validation is ID-based
-    public UserDetails loadUserById(@NonNull UUID id) throws UsernameNotFoundException {
+    @NonNull
+    public UserDetails loadUserById(@NonNull UUID id) throws UsernameNotFoundException, IllegalArgumentException {
+        Assert.notNull(id, "User ID must not be null");
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with ID: " + id));
 
-        return org.springframework.security.core.userdetails.User.builder()
-                .username(user.getId().toString()) 
-                .password("")
-                .build();
+        return Objects.requireNonNull(
+            org.springframework.security.core.userdetails.User.builder()
+                    .username(user.getId().toString())
+                    .password("")
+                    .build(),
+            "unexpectedly built a null UserDetails object for user ID: " + id
+        );
     }
 }
